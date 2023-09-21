607b45208d997a91bba40129f4d4f825e49f9adc
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-21 10:19:59 -0300

    feat: add axios post adapter

---
+++ src/infra/http/axios-http-client/axios-http-post-client.ts
@@ -0,0 +1,12 @@
+import { type HttpResponse, type HttpPostParams, type HttpPostClient } from '@/data/protocols/http'
+import axios from 'axios'
+
+export class AxiosHttPostClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
+  }
+}
