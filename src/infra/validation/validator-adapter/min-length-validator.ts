7ea8e8550f830cb58e345224147fad2ad5debee5
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-22 18:05:03 -0300

    feat: create min length field validator

---
+++ src/infra/validation/validator-adapter/min-length-validator.ts
@@ -0,0 +1,11 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { type FieldValidation } from '@/validation/protocols'
+import validator from 'validator'
+
+export class MinLengthValidator implements FieldValidation {
+  constructor (readonly field: string, private readonly minLength: number) { }
+
+  validate (value: string): Error | null {
+    return validator.isLength(value, { min: this.minLength }) ? null : new InvalidFieldError()
+  }
+}
