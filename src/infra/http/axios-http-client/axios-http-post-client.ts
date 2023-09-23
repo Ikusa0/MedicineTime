afa91298b10d86a5c7e43ec599c30f39b4e123b3
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-23 15:36:17 -0300

    fix: stop axios from throwing errors on request fail

--- src/infra/http/axios-http-client/axios-http-post-client.ts
+++ src/infra/http/axios-http-client/axios-http-post-client.ts
@@ -1,9 +1,17 @@
-import { type HttpResponse, type HttpPostParams, type HttpPostClient } from '@/data/protocols/http'
+import {
+  type HttpResponse,
+  type HttpPostParams,
+  type HttpPostClient
+} from '@/data/protocols/http'
 import axios from 'axios'
 
 export class AxiosHttPostClient implements HttpPostClient<any, any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
+    const httpResponse = await axios.post(
+      params.url,
+      params.body,
+      { validateStatus: () => true }
+    )
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
