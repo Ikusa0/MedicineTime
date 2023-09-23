7455ca4ece380652af570f5def7c032f51e85ff0
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-22 22:08:55 -0300

    chore: change ValidationComposite to recieve an object with validators

--- src/validation/validators/validation-composite.ts
+++ src/validation/validators/validation-composite.ts
@@ -1,11 +1,13 @@
 import { type Validation } from '@/presentation/protocols'
 import { type FieldValidation } from '../protocols'
 
+type ValidatorsType = Record<string, FieldValidation[]>
+
 export class ValidationComposite implements Validation {
-  constructor (private readonly validators: FieldValidation[]) {}
+  constructor (private readonly validators: ValidatorsType) {}
 
   validate (field: string, value: string): string | null {
-    const validators = this.validators.filter((val) => val.field === field)
+    const validators = this.validators[field]
     for (const validator of validators) {
       const error = validator.validate(value)
       if (error) {
