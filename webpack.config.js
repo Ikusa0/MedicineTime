c821a13f53b92cf3eb169006d3e0f69f2dc73927
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-23 21:39:38 -0300

    chore: add dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,65 @@
 /** @type {import('webpack').Configuration} */
 
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/main/index.tsx',
+  mode: "development",
+  entry: "./src/main/index.tsx",
   output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
+    path: path.join(__dirname, "public/js"),
+    publicPath: "/public/js",
+    filename: "bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss', 'css'],
+    extensions: [".ts", ".tsx", ".js", "scss", "css"],
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
+      "@": path.join(__dirname, "src"),
+    },
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.(s?)css$/,
-      use: [{
-        loader: 'style-loader',
-      }, {
-        loader: 'css-loader',
-        options: {
-          modules: true
-        }
-      }, {
-        loader: 'sass-loader',
-      }],
-      exclude: /node_modules/
-    }, {
-      test: /\.svg$/,
-      loader:'svg-react-loader',
-      exclude: /node_modules/
-    }
-  ]
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(s?)css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-react-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
   devServer: {
     devMiddleware: {
-      writeToDisk: true
+      writeToDisk: true,
     },
-    static: './public',
-    historyApiFallback: true
+    static: "./public",
+    historyApiFallback: true,
   },
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
+    react: "React",
+    "react-dom": "ReactDOM",
   },
-  plugins: [
-    new CleanWebpackPlugin()
-  ]
-}
+  plugins: [new CleanWebpackPlugin(), new Dotenv({ safe: true })],
+};
