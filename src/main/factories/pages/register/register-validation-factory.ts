05213eb143499c7cdc56270941c6af0744cdd806
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-09-29 02:16:17 -0300

    feat: add Register validation factory

---
+++ src/main/factories/pages/register/register-validation-factory.ts
@@ -0,0 +1,13 @@
+import {
+  ValidationBuilder,
+  ValidationComposite
+} from '@/validation/validators'
+
+export const makeRegisterValidation = (): ValidationComposite => {
+  return ValidationComposite.build({
+    name: ValidationBuilder.field('name').required().build(),
+    email: ValidationBuilder.field('email').required().email().build(),
+    password: ValidationBuilder.field('password').required().min(6).build(),
+    passwordConfirmation: ValidationBuilder.field('passwordConfirmation').required().equals('password').build()
+  })
+}
