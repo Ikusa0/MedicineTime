c44e598f8acad1bae150e271dd05d6f0fee1cce0
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-10-04 14:54:50 -0300

    feat:  ensure LocalSaveAccessToken throws if accessToken is falsy

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
 import { type SetStorage } from '@/data/protocols/cache/set-storage'
+import { UnexpectedError } from '@/domain/errors'
 import { type SaveAccessToken } from '@/domain/usecases'
 
 export class LocalSaveAccessToken implements SaveAccessToken {
   constructor (private readonly setStorage: SetStorage) { }
 
   save (accessToken: string): void {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     this.setStorage.set('access_token', accessToken)
   }
 }
