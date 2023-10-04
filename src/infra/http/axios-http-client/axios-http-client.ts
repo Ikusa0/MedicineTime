e2e9b9675d4342175cd1b00d85ddb5082b8c2293
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-10-04 18:16:04 -0300

    feat: make AxiosHttpClient implement HttpGetClient interface

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,9 +1,23 @@
-import { type HttpPostClient, type HttpPostParams, type HttpResponse } from '@/data/protocols/http'
+import {
+  type HttpGetClient,
+  type HttpGetParams,
+  type HttpPostClient,
+  type HttpPostParams,
+  type HttpResponse
+} from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient implements HttpPostClient<any, any> {
+export class AxiosHttpClient implements HttpPostClient<any, any>, HttpGetClient<any> {
   async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
     const httpResponse = await axios.post(params.url, params.body, { validateStatus: () => true })
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
+  }
+
+  async get (params: HttpGetParams): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.get(params.url, { validateStatus: () => true })
     return {
       statusCode: httpResponse.status,
       body: httpResponse.data
