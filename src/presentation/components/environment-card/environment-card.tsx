9a0cb8aa1f0251aac34202ff0a12c4b5029e719c
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-10-16 10:33:12 -0300

    feat: update EnvironmentCard to use skeleton shimmer effect when empty

--- src/presentation/components/environment-card/environment-card.tsx
+++ src/presentation/components/environment-card/environment-card.tsx
@@ -3,15 +3,38 @@ import { Card, EnvironmentIcon } from '@/presentation/components'
 import React from 'react'
 import Styles from './environment-card-styles.scss'
 
-const EnvironmentCard: React.FC<EnvironmentModel> = (props: EnvironmentModel) => {
+function isEnvironmentModel (obj: any): obj is EnvironmentModel {
+  return 'id' in obj && 'name' in obj && 'address' in obj && 'iconName' in obj
+}
+
+const EnvironmentCard: React.FC<EnvironmentModel | object> = (props: EnvironmentModel | object) => {
+  if (isEnvironmentModel(props)) {
+    const { name, address, iconName } = props
+    return (
+      <Card>
+        <div className={Styles.content}>
+          <div className={Styles.iconWrapper}>
+            <EnvironmentIcon iconName={iconName} className={Styles.icon} />
+            <h3>{name}</h3>
+          </div>
+          <span>{address}</span>
+        </div>
+      </Card>
+    )
+  }
+
   return (
     <Card>
-      <div className={Styles.content}>
+      <div className={`${Styles.content} ${Styles.empty}`}>
         <div className={Styles.iconWrapper}>
-          <EnvironmentIcon iconName={props.iconName} className={Styles.icon} />
-          <h3>{props.name}</h3>
+          <div className={Styles.icon}></div>
+          <div className={Styles.text}></div>
         </div>
-        <span>{props.address}</span>
+        <div className={Styles.subTextWrapper}>
+          <div className={Styles.subText}></div>
+          <div className={Styles.subText}></div>
+          <div className={Styles.subText}></div>
+        </div>
       </div>
     </Card>
   )
