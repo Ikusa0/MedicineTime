5d2f0b38977fe47df202f9f46ffaaf071074a788
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-10-17 09:17:36 -0300

    feat: add GetCurrentAccount to AuthContext

--- src/presentation/contexts/auth-context.ts
+++ src/presentation/contexts/auth-context.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react'
 
 type AuthContextTypes = {
   updateCurrentAccount: (account: AccountModel) => void
+  getCurrentAccount: () => AccountModel
 }
 
 export const AuthContext = createContext<AuthContextTypes | null>(null)
