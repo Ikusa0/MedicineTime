61fc2892dbb3f37542bc471bc1e5cdcb0d0e1957
Ayrton Porto <22572595+Ikusa0@users.noreply.github.com>
2023-11-21 03:08:25 -0300

    feat: add logoff button

--- src/presentation/components/dropdown-list/dropdown-list.tsx
+++ src/presentation/components/dropdown-list/dropdown-list.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { AuthContext } from '@/presentation/contexts'
+import React, { useContext } from 'react'
 import { BiSolidExit as ExitIcon } from 'react-icons/bi'
+import { useNavigate } from 'react-router-dom'
 import Styles from './dropdown-list-styles.scss'
 
 const DropdownList: React.FC = () => {
+  const { updateCurrentAccount } = useContext(AuthContext)!
+  const navigate = useNavigate()
+
+  const logoff = (): void => {
+    updateCurrentAccount()
+    navigate('/login')
+  }
+
   return (
     <div className={Styles.dropdownList}>
       <div className={Styles.arrowUp}></div>
       <ul className={Styles.listWrapper}>
-        <li className={Styles.listItem}>
+        <li onClick={logoff} className={Styles.listItem}>
           <ExitIcon className={Styles.icon} />
           <span className={Styles.title}>Sair</span>
         </li>
